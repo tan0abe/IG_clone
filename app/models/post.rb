bad137e1356f04e13afb781915f7ca7b36b611ec
ryota tanabe <teni0abe@gmail.com>
2019-04-17 17:55:31 +0900

    [update]postモデルにバリデーションを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, length: { minimum: 1 }
+  validates :title, length: { maximum: 30 }
+  validates :content, length: { minimum: 1 }
+  validates :content, length: { maximum: 140 }
 end
