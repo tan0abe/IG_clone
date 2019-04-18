0afa264540fb8e223472573d0a9d6858efb372e9
ryota tanabe <teni0abe@gmail.com>
2019-04-18 11:15:14 +0900

    [update]postsコントローラのprivateにpost_paramsメソッドを設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,4 +10,10 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :image_cache) #image_cacheは画像のデータそのものを取り扱うパラメータで、確認画面の実装を挟む時などに使用する
+  end
 end
