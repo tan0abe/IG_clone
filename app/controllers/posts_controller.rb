6e8bf71d252aebb31357e64ccf7ebf36a38db58e
ryota tanabe <teni0abe@gmail.com>
2019-04-18 12:05:50 +0900

    [update]postsコントローラのcreateアクションの作成&post_paramsの修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,16 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "投稿しました"
+      redirect_to posts_path
+    else
+      render "new"
+    end
+  end
+
   def index
     @posts = Post.all
   end
@@ -14,6 +24,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:image, :image_cache) #image_cacheは画像のデータそのものを取り扱うパラメータで、確認画面の実装を挟む時などに使用する
+    params.require(:post).permit(:title, :content, :image_path, :image_cache) #image_cacheは画像のデータそのものを取り扱うパラメータで、確認画面の実装を挟む時などに使用する
   end
 end
