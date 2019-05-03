0c6ca9b364e700bae611a9f1d92d6e93580bb49a
ryota tanabe <teni0abe@gmail.com>
2019-05-03 17:18:44 +0900

    [update]CSSの修正&Bootstrapでボタンを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_22_090642) do
+ActiveRecord::Schema.define(version: 2019_05_03_080710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_04_22_090642) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_path"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
