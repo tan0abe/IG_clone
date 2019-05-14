2fbdf0e6cf54306bf14fbf9b339eae8f270c3cbf
ryota tanabe <teni0abe@gmail.com>
2019-05-14 17:47:03 +0900

    generate rspec:installの実行 & いらないファイルの自動生成をしないようにした

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,16 @@ module FBClone
       g.assets false
       g.helper false
     end
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
